a53a1f42fd7ccab4762f254c11a27ce8701557db
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-03 09:47:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import streamlit_shadcn_ui as ui
 import streamlit_antd_components as sac
 
 
+r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
+
+
+
 
 streamlit_extras.let_it_rain.rain('•', 20, falling_speed=5, animation_length="infinite")
 
